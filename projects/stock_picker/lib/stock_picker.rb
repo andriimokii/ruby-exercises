28d4df897b58086569b6a0ae23c65a936a218621
Andrii Mokii <andriimokii@gmail.com>
2022-09-07 22:15:55 +0300

    07-09-22: make fix stock picker

--- projects/stock_picker/lib/stock_picker.rb
+++ projects/stock_picker/lib/stock_picker.rb
@@ -1,12 +1,6 @@
 def stock_picker(array)
-  result = [0, 0]
-
-  array.each_with_index do |stock, index|
+  array.each_with_index.reduce(Array.new(2, 0)) do |result, (stock, index)|
     max_sell = array[index..].max
-    if max_sell - stock > array[result[1]] - array[result[0]]
-      result[0], result[1] = index, array.index(max_sell)
-    end
+    max_sell - stock > array[result.last] - array[result.first] ? [index, array.index(max_sell)] : result
   end
-
-  result
 end
